142574b9779bc43367e146349ebfe427237a66ee
FredericoSRamos <106923488+FredericoSRamos@users.noreply.github.com>
2025-01-21 03:19:00 -0300

    Update mongo uri

--- BackEnd/config.js
+++ BackEnd/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     "secretKey": '72345-96235-37101-28430',
-    "mongoUrl": 'mongodb+srv://fred:qwermnbv@cluster0.kpc1h.mongodb.net/cargoshop?retryWrites=true&w=majority&appName=Cluster0'
+    "mongoUrl": 'mongodb+srv://fred:qwermnbv@cluster0.jhbnp.mongodb.net/cargoshop?retryWrites=true&w=majority&appName=Cluster0'
 }
